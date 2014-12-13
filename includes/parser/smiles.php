3b4ed1260b131b96940154e5081d797817a37235
Electric-Lizard <Koldasov3@gmail.com>
2014-12-13 21:45:44 +0400

    removing unwanted files

--- includes/parser/smiles.php
+++ includes/parser/smiles.php
@@ -3,7 +3,6 @@ $d = "/chatbox/temps/includes/smiles";
 $mainSmiles = array(
 	array("code" => ":)", "src" => "$d/smiley.gif", "title" => "Улыбаюсь"),
 	array("code" => ":(", "src" => "$d/sad.gif", "title" => "Sad"),
-	array("code" => ";)", "src" => "$d/wink.gif", "title" => "Подмигиваю"),
 	array("code" => ":D", "src" => "$d/cheesy.gif", "title" => "Валяюсь"),
 	array("code" => ":o", "src" => "$d/shocked.gif", "title" => "Shocked"),
 	array("code" => "???", "src" => "$d/huh.gif", "title" => "Huh"),
